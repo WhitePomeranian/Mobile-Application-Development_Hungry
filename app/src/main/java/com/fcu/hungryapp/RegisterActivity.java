d72cdd7a530d5e69069433771928d2fad740927c
WhitePomeranian <bzc2551549@gmail.com>
2024-05-21 23:50:33 +0800

    註冊到登入的連結之程式碼被誤刪，將其補回

--- app/src/main/java/com/fcu/hungryapp/RegisterActivity.java
+++ app/src/main/java/com/fcu/hungryapp/RegisterActivity.java
@@ -1,7 +1,13 @@
 package com.fcu.hungryapp;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -114,7 +120,29 @@ public class RegisterActivity extends AppCompatActivity {
                 });
             }
         });
-    }
+
+
+        tvRegisterLogin = findViewById(R.id.tv_register_login);
+
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setUnderlineText(true); // 設置底線
+                ds.setColor(Color.parseColor("#4BB2F9"));
+            }
+        };
+            SpannableString spannableString = new SpannableString(tvRegisterLogin.getText().toString());
+            spannableString.setSpan(clickableSpan, 0, spannableString.length(), Spanned.SPAN_INCLUSIVE_INCLUSIVE); // 設置監聽器並包含頭尾兩字
+            tvRegisterLogin.setText(spannableString);
+            tvRegisterLogin.setMovementMethod(LinkMovementMethod.getInstance()); // 使TextView中的超連結能夠被點擊
+        }
 
     private void write_data(String email, String password, String name, String phone) {
 
