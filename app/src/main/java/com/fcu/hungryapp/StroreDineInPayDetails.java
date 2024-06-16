8c8433a97a8ed86dfdf011a3e9b8f65edadce97a
WhitePomeranian <bzc2551549@gmail.com>
2024-06-16 19:29:42 +0800

    修正檔案名稱

--- app/src/main/java/com/fcu/hungryapp/StroreDineInPayDetails.java
+++ app/src/main/java/com/fcu/hungryapp/StroreDineInPayDetails.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Store_dine_in_details extends AppCompatActivity {
+public class StroreDineInPayDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_store_dine_in_details);
+        setContentView(R.layout.activity_strore_dine_in_pay_details);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
